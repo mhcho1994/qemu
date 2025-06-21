63195f03eb563a5772f00ae2135346a08aaf30a7
Arslan8 <arslankhan52@gmail.com>
2025-06-21 00:36:33 -0400

    Hello M7

---
+++ include/hw/arm/cortexm7_soc.h
@@ -0,0 +1,54 @@
+/*
+ * CORTEXM7 SoC
+ *
+ * Copyright (c) 2014 Alistair Francis <alistair@alistair23.me>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef HW_ARM_CORTEXM7_SOC_H
+#define HW_ARM_CORTEXM7_SOC_H
+
+#include "hw/or-irq.h"
+#include "hw/arm/armv7m.h"
+#include "qom/object.h"
+
+#define TYPE_CORTEXM7_SOC "cortexm7-soc"
+OBJECT_DECLARE_SIMPLE_TYPE(CORTEXM7State, CORTEXM7_SOC)
+
+
+#define FLASH_BASE_ADDRESS 0x00000000
+#define FLASH_SIZE (512 *1024 * 1024)
+#define SRAM_SIZE (512 * 1024 * 1024)
+#define SRAM_BASE_ADDRESS 0x20000000
+
+struct CORTEXM7State {
+    SysBusDevice parent_obj;
+
+    ARMv7MState armv7m;
+
+    MemoryRegion sram;
+    MemoryRegion flash;
+    MemoryRegion flash_alias;
+
+    Clock *sysclk;
+    Clock *refclk;
+};
+
+#endif
